a9e073cba012ee36886342825a958ad410fda074
Travis Franklin <travis.william.franklin@gmail.com>
2020-02-04 13:54:21 -0500

    moves axios call to app.js class component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import axios from 'axios';
+import { tsConstructorType } from '@babel/types';
+const API_ENDPOINT = 'https://swapi.co/api/people/1/';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'not the name'
+    }
+  }
+  componentDidMount(){
+    axios.get(API_ENDPOINT)
+    .then(response => {
+        console.log(response.data.name);
+        this.setState({
+          name: response.data.name
+        })
+    });
+
+  }
+    render(){
+      return (
+        <div className="App">
+          <header className="App-header">
+            {this.state.name}
+          </header>
+        </div>
+      );
+  }
+  
+};
+
+
 
 export default App;
